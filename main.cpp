fcd16ea41b9003bf2911b82200e5ebe2ae3d1d4e
Aryan Chaubal <f20190130@hyderabad.bits-pilani.ac.in>
2022-02-20 16:54:55 +0530

    Exceptions are now printed properly with color

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 int main(int argc, char **argv) {
+
     if (argc != 2) {
         std::cout << "Usage: ./lexer <input file>" << std::endl;
         return 1;
@@ -19,9 +20,13 @@ int main(int argc, char **argv) {
             std::cout << token->lineNumber << "\t" << token->tokenString << "\t" << token->type << std::endl;
         }
     } catch (FileNotFoundException e) {
+        std::cout << "\033[1;31mFileNotFoundException\033[0m  ";
         std::cout << "File not found: " << e.getFileName() << std::endl;
     } catch (LexicalException e) {
-        std::cout << "Lexical error on line " << e.getLineNumber() << ": " << e.getMessage() << std::endl;
+        std::cout << "\033[1;31mLexicalException\033[0m  ";
+        std::cout << "Lexical error on line " << e.getLineNumber() << std::endl << std::endl;
+        std::cout << "\033[1;32m"  << e.getLineNumber() << " \033[0m" << e.getLine() << std::endl;
+        std::cout << "\033[1;36mMessage: \033[0m" << e.getMessage() << std::endl;
     }
     return 0;
 }
