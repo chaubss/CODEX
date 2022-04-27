aa8a99a426f97ac1222e637d50a8488254c4b0d3
BOISaMmY <samarth16022002@gmail.com>
2022-04-27 14:36:11 +0530

    updated lexer

--- lexer.cpp
+++ lexer.cpp
@@ -509,7 +509,6 @@ void Lexer::addToken(std::string token, int line, std::string type) {
     // Checking if current token has an entry in symbol table. If not, creating new entry.
     if (symbolTable.find(token) == symbolTable.end()) symbolTable[token] = newSymbolID++;
     Token t = Token(token, line, type, symbolTable[token]);
-    Token t = Token(token, line, type, 1);    //Token is user defined data type
 
     tokens.push_back(t);
 }
