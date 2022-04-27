a17d792a56926c5c23ba97ce75d1d948496dc3a5
Aryan Chaubal <f20190130@hyderabad.bits-pilani.ac.in>
2022-04-27 13:50:07 +0530

    Added: for loop and if else

---
+++ lexer.mk
@@ -0,0 +1,14 @@
+lexer : main_lexer.o lexer.o token.o exceptions.o
+	g++ main_lexer.o lexer.o token.o exceptions.o -o lexer
+
+main_lexer.o : main_lexer.cpp parser.h ptable_reader.h lexer.h token.h exceptions.h
+	g++ -c main_lexer.cpp
+
+lexer.o : lexer.cpp lexer.h
+	g++ -c lexer.cpp
+
+token.o : token.cpp token.h
+	g++ -c token.cpp
+
+exceptions.o : exceptions.cpp exceptions.h
+	g++ -c exceptions.cpp
---
+++ main_lexer.cpp
@@ -0,0 +1,45 @@
+#include "lexer.h"
+#include "exceptions.h"
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        std::cout << "Usage: ./lexer <input file>" << std::endl;
+        return 1;
+    }
+    std::string fileName = argv[1];
+    try {
+        Lexer lex = Lexer(fileName);
+        while (true) {
+            Token *token = lex.getNextToken();   //fetch stored tokens from vector tokens
+            if (token == NULL) {       // NULL will be returned if request exceeded the stored tokens
+                break;                 // to break the while loop
+            }
+            std::cout << std::left << "Token " << std::setw(7) << token->tokenId << "Line " << std::setw(6) << token->lineNumber << std::setw(15) << token->type << token->tokenString << std::endl << std::right;
+        }
+
+    } catch (FileNotFoundException e) {
+        std::cout << "\033[1;31mFileNotFoundException\033[0m  ";
+        std::cout << "File not found: " << e.getFileName() << std::endl;
+    } catch (LexicalException e) {
+        std::cout << "\033[1;31mLexicalException\033[0m  ";
+        std::cout << "Lexical error on line " << e.getLineNumber() << std::endl << std::endl;
+        std::cout << "\033[1;32m"  << e.getLineNumber() << " \033[0m" << e.getLine();
+        std::cout << "  ";
+        int lineNumber = e.getLineNumber();
+        while (lineNumber > 9) {
+            lineNumber /= 10;
+            std::cout << " ";
+        }
+        for (int i = 0; i < e.getLinePointer(); i++) {
+            std::cout << " ";
+        }
+        std::cout << "\033[1;31m^\033[0m" << std::endl;
+        std::cout << "\033[1;36mMessage: \033[0m" << e.getMessage() << std::endl;
+
+    }
+    return 0;
+}
