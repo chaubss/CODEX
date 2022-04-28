787ceeffba4a6dbd3e1336182210e683664e40f0
BOISaMmY <samarth16022002@gmail.com>
2022-04-28 16:20:30 +0530

    Updated csv and rule list with new rules

--- get_values.py
+++ get_values.py
@@ -6,10 +6,12 @@ with (open('grammar.txt', 'r')) as f:
         g = g.strip()
         g = g.split('->')
         name = g[0]
+        name = name.strip()
         g = g[1]
         g = g.strip()
         num = 0
         if (g != "''"):
             g = g.split(' ')
             num = len(g)
-        print(f'{name} {num}')
+        # print(f'{name}{num}')
+        print("{ \""+name+"\", \"" + str(num) + "\" },")
