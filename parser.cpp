50c682fd8a39a10a3e0853504fc7b5133c5db417
Aryan Chaubal <f20190130@hyderabad.bits-pilani.ac.in>
2022-04-28 23:35:44 +0530

    Final changes

--- parser.cpp
+++ parser.cpp
@@ -21,7 +21,6 @@ void  Parser::modifyStack(std::vector<SemanticObject> &stack, int ruleNumber) {
     bool toPush = true;
     switch (ruleNumber) {
         case 0:
-        std::cout << "asdf" << std::endl;
         toPush = false;
         break;
         case 1: {
@@ -42,20 +41,82 @@ void  Parser::modifyStack(std::vector<SemanticObject> &stack, int ruleNumber) {
         case 8:
         toPush = false;
         break;
+        case 9:
+        toPush = false;
+        break;
+        case 10:
+        toPush = false;
+        break;
+        case 11: {
+            SemanticObject defnList = stack.back();
+            stack.pop_back(); stack.pop_back();
+            so.temp = "";
+            so.code = defnList.code;
+            break;
+        }
+        case 12: {
+            SemanticObject defnList = stack.back();
+            stack.pop_back(); stack.pop_back();
+            SemanticObject imi = stack.back();
+            stack.pop_back();
+            so.temp = "";
+            so.code = imi.code + "\n" + defnList.code;
+            break;
+        }
+        case 13: {
+            SemanticObject imi = stack.back();
+            stack.pop_back();
+            so.code = imi.code;
+            so.temp = imi.temp;
+            break;
+        }
+        case 14: {
+            SemanticObject id = stack.back();
+            stack.pop_back();
+            so.code = id.code;
+            so.temp = id.temp;
+            break;
+        }
+        case 15: {
+            SemanticObject expr = stack.back();
+            stack.pop_back(); stack.pop_back();
+            SemanticObject id = stack.back();
+            stack.pop_back();
+            so.code = expr.code + "\n" + id.temp + " := " + expr.temp;
+            break;
+        }
+        case 16: {
+            stack.pop_back();
+            so.code = stack.back().code;
+            stack.pop_back(); stack.pop_back(); stack.pop_back(); stack.pop_back(); stack.pop_back(); stack.pop_back();
+            break;
+        }
+        case 21: {
+            stack.pop_back();
+            SemanticObject stmt2 = stack.back();
+            stack.pop_back(); stack.pop_back(); stack.pop_back(); stack.pop_back();
+            SemanticObject stmt1 = stack.back();
+            stack.pop_back(); stack.pop_back(); stack.pop_back();
+            SemanticObject expr = stack.back();
+            stack.pop_back(); stack.pop_back(); stack.pop_back();
+            so.temp = "";
+            std::string ifL = generateLabelToken();
+            std::string elseL = generateLabelToken();
+            std::string skipL = generateLabelToken();
+
+
+            so.code = expr.code + "\nif(" + expr.temp + ") goto " + ifL + "\ngoto " + elseL + "\n" + ifL + ":\n" + stmt1.code + "\ngoto " + skipL + "\n" + elseL + ":\n" + stmt2.code + "\n" + skipL + ":\n";
+            break;
+        }
         case 23: {
-            SemanticObject _ = stack.back();
             stack.pop_back();
             SemanticObject stmts = stack.back();
             stack.pop_back();
-            SemanticObject __ = stack.back();
             stack.pop_back();
-            SemanticObject ___ = stack.back();
             stack.pop_back();
             SemanticObject expr = stack.back();
             stack.pop_back();
-            SemanticObject ____ = stack.back();
             stack.pop_back();
-            SemanticObject _____ = stack.back();
             stack.pop_back();
             so.temp = "";
             std::string ifL = generateLabelToken();
@@ -72,9 +133,6 @@ void  Parser::modifyStack(std::vector<SemanticObject> &stack, int ruleNumber) {
             stack.pop_back();
             so.temp = "";
             so.code = expr.code + "\n" + id.temp + " := " + expr.temp;
-            std::cout << "******************" << std::endl;
-            std::cout << so.code << std::endl;
-            std::cout << "******************" << std::endl;
             break;
         }
         case 28: {
@@ -162,9 +220,6 @@ void  Parser::modifyStack(std::vector<SemanticObject> &stack, int ruleNumber) {
             stack.pop_back();
             so.code = op1.code + "\n" + op2.code + "\n";
             so.code += so.temp + " := " + op1.temp + " " + op.temp + " " + op2.temp;
-            std::cout << "-----------------------------------------" << std::endl;
-            std::cout << so.code << std::endl;
-            std::cout << "-----------------------------------------" << std::endl;
             break;
         }
         case 36: {
@@ -232,6 +287,36 @@ void  Parser::modifyStack(std::vector<SemanticObject> &stack, int ruleNumber) {
         case 45:
         toPush = false;
         break;
+        case 46:
+        toPush = false;
+        break;
+        case 47: {
+            stack.pop_back();
+            SemanticObject arg = stack.back();
+            stack.pop_back(); stack.pop_back(); stack.pop_back();
+            so.code = arg.code;
+            break;
+        }
+        case 48: {
+            SemanticObject args = stack.back();
+            stack.pop_back(); stack.pop_back();
+            SemanticObject arg = stack.back();
+            stack.pop_back();
+            so.code = arg.code + "\n" + args.code;
+            break;
+        }
+        case 49: {
+            SemanticObject arg = stack.back();
+            stack.pop_back();
+            so.code = arg.code;
+            break;
+        }
+        case 50: {
+            SemanticObject expr = stack.back();
+            stack.pop_back();
+            so.code = expr.code;
+            break;
+        }
         case 58: {
             so.temp = stack.back().temp;
             so.code = "";
@@ -284,27 +369,27 @@ void  Parser::modifyStack(std::vector<SemanticObject> &stack, int ruleNumber) {
             so.temp = "";
             so.code = "";
             int toRemove = stoi(preader->ruleConstants[ruleNumber][1]);
+            for (int i = stack.size() - toRemove; i < stack.size(); i++) {
+                if (stack[i].code != "") {
+                    so.code += stack[i].code + "\n";
+                }
+            }
             for (int i = 0; i < toRemove; i++) {
                 stack.pop_back();
             }
         }
     }
-    // if (so.code != "" && so.temp != "") {
-    //     stack.push_back(so);
-    //     std::cout << "Pushing " << stack.back().code << std::endl;
-    // }
     if (toPush) {
         stack.push_back(so);
-        std::cout << "Pushing " << ((stack.back().code == "") ? "<empty>" : stack.back().code) << " " << ((stack.back().temp == "") ? "<empty>" : stack.back().temp) << std::endl;
     }
 }
 
 void printStack(std::vector<std::string> &s, std::string move){
-    // std::cout<<"Stack: ";
-    // for(int i = 0; i < s.size(); i++){
-    //     std::cout<<s[i]<<" ";
-    // }
-    // std::cout<<std::endl<<move<<std::endl;
+    std::cout<<"Stack: ";
+    for(int i = 0; i < s.size(); i++){
+        std::cout<<s[i]<<" ";
+    }
+    std::cout<<std::endl<<move<<std::endl;
 }
 
 std::string resolveTokenValue(Token t){
@@ -326,51 +411,11 @@ void printErrors(std::vector<std::vector<std::string> > &errorList){
     }
 }
 
-/*
-27 ASSIGN -> ID = EXPR { ID.place = EXPR.place }
-//////////////////////////////////////////////////////////////////////////////////
-28 EXPR -> EXPR == TERM { ti = genTemp(); ti = EXPR.place == TERM.place }
-29 EXPR -> EXPR != TERM { ti = genTemp(); ti = EXPR.place != TERM.place }
-30 EXPR -> EXPR > TERM { ti = genTemp(); ti = EXPR.place > TERM.place }
-31 EXPR -> EXPR < TERM { ti = genTemp(); ti = EXPR.place < TERM.place }
-32 EXPR -> EXPR >= TERM { ti = genTemp(); ti = EXPR.place >= TERM.place }
-33 EXPR -> EXPR <= TERM { ti = genTemp(); ti = EXPR.place <= TERM.place }
-///////////////////////////////////////////////////////////////////////////////////
-34 EXPR -> TERM { EXPR.place = TERM.place }
-///////////////////////////////////////////////////////////////////////////////////
-35 TERM -> TERM + TERM2 { ti = genTemp(); ti = TERM.place + TERM2.place; }
-36 TERM -> TERM - TERM2 { ti = genTemp(); ti = TERM.place - TERM2.place; }
-////////////////////////////////////////////////////////////////////////////////////
-37 TERM -> TERM2 { TERM.place = TERM2.place }
-////////////////////////////////////////////////////////////////////////////////////
-38 TERM2 -> TERM2 * TERM3 { ti = genTemp(); ti = TERM2.place * TERM3.place; }
-39 TERM2 -> TERM2 / TERM3 { ti = genTemp(); ti = TERM2.place / TERM3.place; }
-////////////////////////////////////////////////////////////////////////////////////
-40 TERM2 -> TERM3 { TERM2.place = TERM3.place }
-41 TERM3 -> ( EXPR ) { TERM3.place = EXPR.place }
-42 TERM3 -> ID { TERM3.place = ID.place }
-43 TERM3 -> NUM { TERM3.place = NUM.place }
-44 TERM3 -> STR { TERM3.place = STR.place }
-45 TERM3 -> FLT { TERM.place = FLT.place }
-////////////////////////////////////////////////////////////////////////////////////
-58 ID -> <id> { ID.place = tokenString; }
-NUM -> <num>
-NUM -> + <num>
-NUM -> - <num>
-STR -> <str>
-FLT -> <flt>
-FLT -> + <flt>
-FLT -> - <flt>
-*/
-
-
 Parser::Parser(std::vector<Token> tokens, ParsingTableReader *ptable) {
-// std::string tokenString, int lineNumber, std::string type, int tokenId
     preader = ptable;
     tokens.push_back(Token("$", -1, "EOF", -1));
     int tokenPointer = 0;
     std::vector<std::string> stack;
-    std::vector<std::string> expressionStack;
     std::vector<SemanticObject> semanticStack;
 
     std::vector<std::vector<std::string> > errorList;
@@ -382,31 +427,21 @@ Parser::Parser(std::vector<Token> tokens, ParsingTableReader *ptable) {
         auto temp = ptable->actionTable.find(make_pair(stack.back(), tokVal));
         if (temp != ptable->actionTable.end()) {
             std::string action = temp->second;
-            // std::cout << "Action: " << action << std::endl;
-            // if (action == "") {
-            //     std::cout << "Error." << std::endl;
-            //     std::cout << "Token: " << tokVal << std::endl;
-            //     return;
-            //     // TODO: error handling
-            // }
             if (action == "acc") {
                 std::cout << "Parsing Successful with ";
                 errorList.size() == 0 ? std::cout<<"no" : std::cout<<"some";
                 std::cout<<" errors."<<std::endl;
                 printErrors(errorList);
+                std::cout << "\n\nThe intermediate three address code generated is:" << std::endl;
                 std::cout << semanticStack.back().code << std::endl;
-                std::cout << semanticStack.size() << std::endl;
                 return;
             }
             if (action[0] == 's') {
                 printStack(stack, action);
                 stack.push_back(tokVal);
                 stack.push_back(action.substr(1));
-                if (token.type == "operator" || token.type == "identifier" || token.type == "operator/sign" || token.type == "int" || token.type == "float" || token.type == "string") {
-                    expressionStack.push_back(token.tokenString);
-                }
                 // push into semantic stack
-                SemanticObject sobj;
+                SemanticObject sobj = SemanticObject();
                 sobj.code = "";
                 sobj.temp = token.tokenString;
                 semanticStack.push_back(sobj);
@@ -414,29 +449,11 @@ Parser::Parser(std::vector<Token> tokens, ParsingTableReader *ptable) {
             } else if (action[0] == 'r') {
                 int ruleNum = stoi(action.substr(1));
                 printStack(stack, action);
-                // std::cout << "Reduce rule " << ruleNum << std::endl;
                 std::string nonTerminal = ptable->ruleConstants[ruleNum][0];
-                // std::cout << "Non-Terminal: " << nonTerminal << std::endl;
                 int ruleLength = stoi(ptable->ruleConstants[ruleNum][1]);
-                // std::cout << "Rule length: " << ruleLength << std::endl;
-                // std::cout << "Stack size: " << stack.size() << std::endl;
                 for (int i = 0; i < 2 * ruleLength; i++) {
                     stack.pop_back();
                 }
-                std::set<int> operationRules {38, 39, 35, 36, 28, 29, 30, 31, 32, 33};
-                if (operationRules.count(ruleNum)) {
-                    std::string secondOperand = expressionStack.back();
-                    expressionStack.pop_back();
-                    std::string operation = expressionStack.back();
-                    expressionStack.pop_back();
-                    std::string firstOperand = expressionStack.back();
-                    expressionStack.pop_back();
-                    std::string temp = generateTemporaryToken();
-                    expressionStack.push_back(temp);
-                    std::string threeCodeRule = temp + " = " + firstOperand + " " + operation + " " + secondOperand + ";";
-                    // std::cout << threeCodeRule << std::endl;
-                }
-                std::cout << "Calling with rule number: " << ruleNum << std::endl;
                 modifyStack(semanticStack, ruleNum);
                 // semantic conditions
                 std::string lastState = stack.back();
@@ -445,7 +462,6 @@ Parser::Parser(std::vector<Token> tokens, ParsingTableReader *ptable) {
                 std::string toPush = ptable->gotoTable[make_pair(lastState, nonTerminal)];
                 printStack(stack, "goto " + toPush);
                 stack.push_back(toPush);
-                // std::cout << "Pushing " << toPush << std::endl;
             }
         } else {
 
@@ -480,10 +496,6 @@ Parser::Parser(std::vector<Token> tokens, ParsingTableReader *ptable) {
                     break;
                 tokenPointer++;                
             }
-            // stack.pop_back();
-            // stack.pop_back();
-
-            // return;
         }
         
     }
