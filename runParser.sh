7ba10afedd8f2d7c1f37658543f8cc64185330b7
BOISaMmY <samarth16022002@gmail.com>
2022-05-05 14:43:22 +0530

    Better merge lines

--- runParser.sh
+++ runParser.sh
@@ -2,6 +2,7 @@ g++ -c exceptions.cpp
 g++ -c token.cpp
 g++ -c lexer.cpp
 g++ -c ptable_reader.cpp
+g++ -c semantic_object.cpp
 g++ -c parser.cpp
 g++ -c main.cpp
-g++ main.o parser.o ptable_reader.o lexer.o token.o exceptions.o -o parser
+g++ main.o parser.o semantic_object.o ptable_reader.o lexer.o token.o exceptions.o -o parser
